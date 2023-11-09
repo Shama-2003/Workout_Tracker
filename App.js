cfddb998aa09c50fa1f90bf2b20ac6e8f04d3cd6
Shama-2003 <107424190+Shama-2003@users.noreply.github.com>
2023-11-09 10:10:46 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container, Paper } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+import Navbar from "./components/Navbar";
+import ExercisesList from "./components/ExercisesList";
+import EditExercise from "./components/EditExercise";
+import CreateExercise from "./components/CreateExercise";
+import CreateUser from "./components/CreateUser";
+
+export default function App() {
+  const useStyles = makeStyles({
+    main: {
+      marginTop: "85px",
+      padding: "20px",
+      height: "60vh"
+    }
+  });
+
+  const classes = useStyles();
+
+  return (
+    <Router>
+      <Navbar />
+      <Container>
+        <Paper className={classes.main} square>
+          <Route path="/" exact component={ExercisesList} />
+          <Route path="/edit/:id" component={EditExercise} />
+          <Route path="/create" component={CreateExercise} />
+          <Route path="/user" component={CreateUser} />
+        </Paper>
+      </Container>
+    </Router>
+  );
+}
